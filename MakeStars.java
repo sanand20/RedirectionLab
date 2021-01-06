b1154827ca03278f988db2f75a5d49cf4ea91b05
Shriya <>
2021-01-05 20:15:47 -0500

    args handled

---
+++ MakeStars.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class MakeStars{
+  public static void main( String[]args ){
+    String in = "";
+    for (int i = 0; i < args.length; i++) {
+        in += args[i];
+    }
+
+      //as the string that you read from
+    
+
+      //use hasNextLine()/nextLine() to loop over
+      //all of the data
+
+      //If you want to read the input word by word
+      //this can be replaced with hasNext() and next()
+
+
+}
+
+}
